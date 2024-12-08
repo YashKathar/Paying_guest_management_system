52b55a369559cb61aa03aaa1c1cc376487a996c5
YashKathar <yashkathar4545@gmail.com>
2024-12-08 18:49:35 +0530

    Initial commit

---
+++ PayingGuestManagementSystem/src/main/java/com/PayingGuestManagementSystem/PayingGuestManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.PayingGuestManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayingGuestManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayingGuestManagementSystemApplication.class, args);
+	}
+
+}
---
+++ PayingGuestManagementSystem/src/test/java/com/PayingGuestManagementSystem/PayingGuestManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PayingGuestManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayingGuestManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
